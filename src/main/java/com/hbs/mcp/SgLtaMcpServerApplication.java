e2292573ea2f58a35d303a19f18c1af0a0014390
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-03 14:34:15 +0800

    Removed the base package for EnableFeignClients annotation

--- src/main/java/com/hbs/mcp/SgLtaMcpServerApplication.java
+++ src/main/java/com/hbs/mcp/SgLtaMcpServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.hbs.mcp.generated.api")
+@EnableFeignClients
 public class SgLtaMcpServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SgLtaMcpServerApplication.class, args);
